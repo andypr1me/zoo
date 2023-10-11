2621174c65d14c126d1ba1922691f1840a808e3b
Andrej Bachor <andrej.bachor@zurich.com>
2023-10-11 10:34:34 +0200

    shortened messages

--- ConvIntToFloat.py
+++ ConvIntToFloat.py
@@ -3,8 +3,8 @@ pi = 3.14
 
 summ = radius + pi
 
-print("Data type of radius is", type(radius))
-print("Data type of pi is", type(pi))
+print("Radius is", type(radius))
+print("Pi is", type(pi))
 
 print("Sum is", summ)
 print("Data type of Sum is", type(summ))
