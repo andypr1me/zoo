ff5e4a26a3b47c784ad7f1e50c0db6cd70816892
Andrej Bachor <andrej.bachor@zurich.com>
2023-10-11 10:01:20 +0200

    New file with conversions of data types

---
+++ ConvIntToFloat.py
@@ -0,0 +1,10 @@
+radius = 22
+pi = 3.14
+
+summ = radius + pi
+
+print("Data type of radius is", type(radius))
+print("Data type of pi is", type(pi))
+
+print("Sum is", summ)
+print("Data type of Sum is", type(summ))
